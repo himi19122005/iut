1c38bf3328517f2cd260f243354e4d93620e6528
unknown <himi19122005@gmail.com>
2026-07-04 01:05:05 +0600

    make a 5 sec on and off timer for all the devices

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template,url_for
 from flask_socketio import SocketIO, emit
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+devices = {
+    "drawingroom_fan1": {"status": "off"},
+    "drawingroom_fan2": {"status": "off"},
+    "drawingroom_light1": {"status": "off"},
+    "drawingroom_light2": {"status": "off"},
+    "drawingroom_light3": {"status": "off"},
 
+    "workroom1_fan1": {"status": "off"},
+    "workroom1_fan2": {"status": "off"},
+    "workroom1_light1": {"status": "off"},
+    "workroom1_light2": {"status": "off"},
+    "workroom1_light3": {"status": "off"},
+
+    "workroom2_fan1": {"status": "off"},
+    "workroom2_fan2": {"status": "off"},
+    "workroom2_light1": {"status": "off"},
+    "workroom2_light2": {"status": "off"},
+    "workroom2_light3": {"status": "off"},
+}
 
 @app.route("/")
 def home():
@@ -15,6 +35,20 @@ def handle_message(data):
 
     emit("chat", f"Server received: {data}") #message is the event name it could be anything else
 
+# run differetn times for different rooms 
+def ticker():
+    while True:
+        for device in devices:
+            devices[device]["status"] = random.choice(["on", "off"])
+
+        socketio.emit(
+            "device_update",
+            devices
+        )
+        socketio.sleep(5)
+
+
 
 if __name__ == "__main__":
+    socketio.start_background_task(ticker)
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,34 @@
 console.log("Connected!");
 const socket = io();
+
 // send a message to the server
-// socket.emit("chat", "Hello server"); if i add this to this page it doesnt work console logging it works 
+// socket.emit("chat", "Hello server"); 
 
   // listen for messages from the server
 socket.on("chat", (msg) => {
     console.log("Message from server:", msg);
+});
+
+// socket.on("tick", (data) => {
+//     document.getElementById("clock").textContent = data.time;
+//     console.log(data);
+// }); delete this 
+
+socket.on("device_update", (devices) => {
+
+    for (const deviceName in devices) {
+        const status = devices[deviceName].status;
+        console.log(status)
+        const card = document.getElementById(deviceName);
+
+        if (!card) continue;
+        card.textContent = status.toUpperCase(); //this only changes the display to uppercase
+
+        if (status === "on") {
+            card.style.backgroundColor = "#daa671";
+        } else {
+            card.style.backgroundColor = "#c55113";
+        }
+    }
+
 });
